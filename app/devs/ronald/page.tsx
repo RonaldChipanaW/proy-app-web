da19aeabed1669f392bd366a71648e5c01cf0106
RonaldChipanaW <ronald.chipana.w@gamail.com>
2026-04-07 16:02:58 -0400

    first commit

---
+++ app/devs/ronald/page.tsx
@@ -0,0 +1,10 @@
+
+export default function RonaldPage() {
+  return (
+    <>
+        <h1 className="text-2xl text-cyan-400 "> Hola desde Pagina Ronald</h1>
+        <p className="text-amber-200">Esta es la pagina de Ronald</p>
+    
+    </>
+  );
+}
