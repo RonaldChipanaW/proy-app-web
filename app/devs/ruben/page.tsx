c4ab0c8eae662947550ddcaf1fac40f0e009d374
rvcruben1-sv <rvcruben1@gmail.com>
2026-04-07 19:57:45 -0400

    carpeta ruben

---
+++ app/devs/ruben/page.tsx
@@ -0,0 +1,10 @@
+
+export default function RonaldPage() {
+  return (
+    <>
+        <h1 className="text-2xl text-cyan-600 "> Hola desde Pagina Ruben</h1>
+        <p className="text-amber-300">Esta es la pagina de Ruben</p>
+    
+    </>
+  );
+}
