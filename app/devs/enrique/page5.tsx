06ac20384a52bd843de734b983bc91612ba2ac46
Quique778 <Enrique_larasalazar@hotmail.com>
2026-04-07 16:24:41 -0400

    Enrique pagina

---
+++ app/devs/enrique/page5.tsx
@@ -0,0 +1,10 @@
+
+export default function EnriquePage() {
+  return (
+    <>
+        <h1 className="text 3xl text-blue-300"> Hola desde esta pagina Quique</h1>
+        <p className="text-red-400">Esta es mi pagina de queques</p>
+        
+    </>
+  );
+}
