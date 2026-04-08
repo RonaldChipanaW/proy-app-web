999257d13c5014fde7fc3c702cd7c82ddfd16ab3
yerko wilian rodriguez molina <yerko@192.168.1.12>
2026-04-08 08:25:12 -0400

    page yerko

---
+++ app/devs/yerko/page.tsx
@@ -0,0 +1,9 @@
+export default function RonaldPage() {
+  return (
+    <>
+        <h1 className="text-2xl text-cyan-600 "> Hola desde yerko </h1>
+        <p className="text-amber-300">Esta es la pagina de yerko</p>
+    
+    </>
+  );
+}
